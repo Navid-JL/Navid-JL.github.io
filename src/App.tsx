c04dc12438ae5eb8ba1da8bb7f22136a676993b3
Navid Jalili <navidjalili3@gmail.com>
2024-11-26 15:41:55 +0330

    Mess around with REACT and Github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>Welcome to my Portfolio</h1>
+        <section>
+          <h2>About Me</h2>
+          <p>Introduction</p>
+        </section>
+        <section>
+          <h2>Projects</h2>
+          <p>Showcase</p>
+        </section>
       </header>
-    </div>
+      <br />
+      <main>Main Stuff</main>
+      <br />
+      <hr />
+      <footer>Footer stuff</footer>
+      <hr />
+    </>
   );
 }
 
